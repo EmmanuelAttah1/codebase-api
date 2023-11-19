7a314a8eb74fff7648c2cc5f4e759645d627bf87
Ubuntu <ubuntu@ip-172-31-34-168.ec2.internal>
2023-11-19 12:42:25 +0000

    Implemented static files

--- mycodebase/settings.py
+++ mycodebase/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-jfgb!k^qkpnnav-0*0w71&y283l*r-^&6$%&0-nk*y$en5d_12
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.90.242.65"]
 
 
 # Application definition
@@ -119,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
